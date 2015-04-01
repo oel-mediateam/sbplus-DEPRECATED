72a5bb2263c04c4839a8cb1751f130b6427f1ac1
Ethan Lin <ethan.lin@uwex.edu>
2015-04-01 18:49:58 -0500

    updated yml and grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,5 +10,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-qunit');
 
     // Task to run tests
-    grunt.registerTask('build', 'qunit');
+    grunt.registerTask('test', 'qunit');
 };
