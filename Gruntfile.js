ff791089bb8940abc2881d5915e522743b013c52
Ethan Lin <ethan.lin@uwex.edu>
2015-04-02 10:16:50 -0500

    Updated and added Grunt test related files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,24 @@
 module.exports = function(grunt) {
+
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+
+        pkg: grunt.file.readJSON( 'package.json' ),
         qunit: {
-            files: ['build/index.html']
+            files: ['build/*.html']
+        },
+        watch: {
+            files: ['build/tests/tests.js', 'build/*.html', 'sources/scripts/storybookplus.js'],
+            tasks: ['qunit'],
         }
+
     });
 
     // Load plugin
-    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
     // Task to run tests
-    grunt.registerTask('test', 'qunit');
+    grunt.registerTask( 'default', 'watch' );
+
 };
