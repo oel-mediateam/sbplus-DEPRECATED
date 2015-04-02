feb4c0b616ab4f1d0c06fbfa786d88b27ec2400b
Ethan Lin <ethan.lin@uwex.edu>
2015-04-01 19:11:39 -0500

    Installed grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         qunit: {
             files: ['build/index.html']
         }
